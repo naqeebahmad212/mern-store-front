30e72e84338b733945b32f5684cf2efcff297f07
naqeebahmad212 <naqeebahmad212@gmail.com>
2024-03-07 14:32:35 +0500

    conflic fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../redux/userSlice/userSlice";
 import productReducer from "../redux/productSlice/productSlice";
 
-export const server = `https://mern-store-back-9h4q.vercel.app/`;
+export const server = `https://mern-store-back-9h4q.vercel.app`;
 
 export const store = configureStore({
   reducer: {
